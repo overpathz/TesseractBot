a2c087b2047bef49e5c1639bade08805795ab370
overpathz <overpathz@gmail.com>
2022-01-30 23:26:14 +0200

    change logic in MessageHandler

--- src/main/java/com/geekglasses/tesseract/handler/MessageHandler.java
+++ src/main/java/com/geekglasses/tesseract/handler/MessageHandler.java
@@ -1,9 +1,9 @@
 package com.geekglasses.tesseract.handler;
 
-import com.geekglasses.tesseract.entity.TagLink;
 import com.geekglasses.tesseract.entity.User;
-import com.geekglasses.tesseract.repo.TagLinkRepo;
+import com.geekglasses.tesseract.entity.UserTag;
 import com.geekglasses.tesseract.repo.UserRepo;
+import com.geekglasses.tesseract.repo.UserTagRepo;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
@@ -11,28 +11,30 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 public class MessageHandler implements Handler<Message> {
 
     private final UserRepo userRepo;
-    private final TagLinkRepo tagLinkRepo;
+    private final UserTagRepo userTagRepo;
 
-    public MessageHandler(UserRepo userRepo, TagLinkRepo tagLinkRepo) {
+    public MessageHandler(UserRepo userRepo, UserTagRepo userTagRepo) {
         this.userRepo = userRepo;
-        this.tagLinkRepo = tagLinkRepo;
+        this.userTagRepo = userTagRepo;
     }
 
     @Override
     public void choose(Message message) {
         if (message.hasText()) {
             String text = message.getText();
+
             if (text.equals("/start")) {
                 User user = new User();
                 user.setChatId(message.getChatId());
+                user.setUsername(message.getChat().getUserName());
                 userRepo.save(user);
             } else if (text.contains("/addtag")) {
                 String desiredTag = text.split(" ")[1].trim();
-                System.out.println(desiredTag);
-                TagLink tagLink = new TagLink();
-                tagLink.setChatId(message.getChatId());
-                tagLink.setTag(desiredTag);
-                tagLinkRepo.save(tagLink);
+
+                UserTag userTag = new UserTag();
+                userTag.setChatId(message.getChatId());
+                userTag.setTag(desiredTag);
+                userTagRepo.save(userTag);
             }
         }
     }
