43641665aaf2698eb5e5527d1820ce241ef4954c
overpathz <overpathz@gmail.com>
2022-01-30 23:25:26 +0200

    add usertagrepo

---
+++ src/main/java/com/geekglasses/tesseract/repo/UserTagRepo.java
@@ -0,0 +1,8 @@
+package com.geekglasses.tesseract.repo;
+
+import com.geekglasses.tesseract.entity.UserTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTagRepo extends JpaRepository<UserTag, Long> {
+
+}
