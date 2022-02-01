e1c50a6b31417539e45bbf438bcbbd07bf7f8eba
overpathz <overpathz@gmail.com>
2022-02-01 18:35:11 +0200

    provide extramethod to UserTagRepo to get all UserTag objects by chatId

--- src/main/java/com/geekglasses/tesseract/repo/UserTagRepo.java
+++ src/main/java/com/geekglasses/tesseract/repo/UserTagRepo.java
@@ -3,6 +3,8 @@ package com.geekglasses.tesseract.repo;
 import com.geekglasses.tesseract.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserTagRepo extends JpaRepository<UserTag, Long> {
+import java.util.List;
 
+public interface UserTagRepo extends JpaRepository<UserTag, Long> {
+    List<UserTag> findAllByChatId(Long chatId);
 }
