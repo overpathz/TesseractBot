979900e927e96b6836113d14c2ed1067b9665947
overpathz <overpathz@gmail.com>
2022-02-01 18:44:20 +0200

    better exception handling

--- src/main/java/com/geekglasses/tesseract/util/TestTesseractRestService.java
+++ src/main/java/com/geekglasses/tesseract/util/TestTesseractRestService.java
@@ -1,29 +1,35 @@
 package com.geekglasses.tesseract.util;
 
 import com.geekglasses.tesseract.model.Article;
+import com.geekglasses.tesseract.task.PostCheckerRunnable;
 import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class TestTesseractRestService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestTesseractRestService.class);
+
     private static final String HABR_ARTICLES = "http://localhost:8080/articles";
+    private static int cachedPostId = 648648;
 
-    private static int cachedPostId = -0;
-
-    @SneakyThrows
     public Article getArticle() {
         List<String> tags = new ArrayList<>(List.of("c++", "java"));
         return new Article("", "https://habr.com/ru/company/luxoft/news/t/585650/", -1, tags);
     }
 
-    @SneakyThrows
+
     public boolean isNewPost() {
         int lastArticleId = getLastArticleId();
         if (lastArticleId != cachedPostId) {
@@ -34,9 +40,15 @@ public class TestTesseractRestService {
         return false;
     }
 
-    @SneakyThrows
+
     private int getLastArticleId() {
-        Document page = Jsoup.connect(HABR_ARTICLES).get();
+        Document page = null;
+        try {
+            page = Jsoup.connect(HABR_ARTICLES).get();
+        } catch (IOException e) {
+            LOGGER.error("Can not get Document by get() method", e);
+        }
+        Objects.requireNonNull(page, "Parsed Document object should be not null");
         Elements articles = page.getElementsByClass("tm-articles-list__item");
         Element article = articles.get(0);
         return Integer.parseInt(article.attributes().get("id"));
